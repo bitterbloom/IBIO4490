1347a5391f192c8fbddb9dedf2c9da9e3bd06206
Adrian Sebastian Volcinschi Moros <bitterbloomed@gmail.com>
2019-02-22 20:49:11 -0500

    Ya no descarga la base de datos completa

--- 03-Python/run.py
+++ 03-Python/run.py
@@ -5,17 +5,28 @@ import numpy as np
 from random import randint
 import os
 import matplotlib.pyplot as plt
-import cv2
 from PIL import ImageDraw, Image
 from shutil import copyfile, rmtree
 import time
+import urllib
+import zipfile
+import ipdb
 
-if not(os.path.isdir("./wiki")):
-	os.system("wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/wiki.tar.gz")
-	os.system("tar -xvf wiki.tar.gz")
+if not(os.path.isdir("./00")):
+
+	url = 'https://www.dropbox.com/s/l5rq8s0baedu44f/wiki.zip?dl=1'
+	u = urllib.request.urlopen(url)
+	data = u.read()
+	u.close()
+	with open(os.getcwd() + '/' + 'wiki.zip', "wb") as f :
+	    f.write(data)
+	f.close()
+
+	zip_ref = zipfile.ZipFile(os.getcwd() + '/' + 'wiki.zip', 'r')
+	zip_ref.extractall(os.getcwd())
+	zip_ref.close()
 
 start = time.time()
-os.chdir("wiki")
 os.mkdir("./temp")
 mat = scipy.io.loadmat('wiki.mat')
 data = mat['wiki'][0,0].tolist()
@@ -23,33 +34,33 @@ data = mat['wiki'][0,0].tolist()
 numFotos = randint(7,17)
 elegido = []
 fotos = 0
+path_00 = os.listdir("./00")
 while fotos < numFotos:
 
-	rint = randint(0,len(data[0][0])+1)
-	path = data[2][0,rint]
+	rint = randint(0,600)
+	path = path_00[rint]
 
-	im = plt.imread(f"{path[0]}")
+	im = plt.imread(f"./00/{path}")
 	if im.shape == (1,1,4):
 		pass
 	else:
-		copyfile(str(path[0]), f"./temp/pic_{fotos}.jpg")
+		copyfile(f"./00/{path}", f"./temp/pic_{fotos}.jpg")
 		fotos += 1
 		elegido.append(rint)
 
 empezo = True
 for i in elegido:
-	
-	if data[3][0,i] == 1:
+	path = path_00[i]
+	fila, num= np.where(data[2]==f"00/{path}")
+	if data[3][0,num] == 1:
 		gender = "Male"
 	else:
 		gender = "Female"
 
-	path = data[2][0,i]
-	init = str(path[0])
-	split = init.split("_")
-	age = int(data[1][0,i])-int(split[1][0:4])
+	split = path.split("_")
+	age = int(data[1][0,num])-int(split[1][0:4])
 
-	im = Image.open(path[0]).convert("L")
+	im = Image.open(f"./00/{path}").convert("L")
 	imResize = im.resize((256,256))
 
 	dwp = ImageDraw.Draw(imResize)
